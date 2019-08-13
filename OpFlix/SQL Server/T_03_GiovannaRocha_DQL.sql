8b12be63cf231ddac6a657e6f9013e5ccd40464b
Giovanna Rocha Barros <43706052881@info.local>
2019-08-13 17:23:47 -0300

    up

---
+++ OpFlix/SQL Server/T_03_GiovannaRocha_DQL.sql
@@ -0,0 +1,33 @@
+use T_OpFlix
+
+select * from Usuarios
+select * from TipoUsuarios
+
+select * from Lancamentos 
+select * from Categorias
+select * from Classificacoes
+select * from Identificacoes
+
+
+select U.Nome, TU.*
+from Usuarios as U
+join TipoUsuarios as TU
+on U.IdTipoUsuario = TU.IdTipoUsuario
+
+select L.Titulo, C.*
+from Lancamentos as L
+join Categorias as C
+on C.IdCategoria = L.IdCategoria
+
+select L.Titulo, Classificacoes.*
+from Lancamentos as L
+join Classificacoes
+on Classificacoes.IdClassificacao = L.IdClassificacao
+
+select L.Titulo, Identificacoes.*
+from Lancamentos as L
+join Identificacoes
+on Identificacoes.IdIdentificacao = L.IdIdentificacao
+
+
+
